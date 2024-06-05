d76f54fe8b84a1a83aca11c681d9f58d4fb975cc
Ader817 <ader05817@gmail.com>
2024-06-05 23:29:10 +0800

    Updated port definition for module Keyboardcontrol,UIDisplay,VGADisplay

--- Code/sources/KeyboardControl.v
+++ Code/sources/KeyboardControl.v
@@ -1,3 +1,12 @@
-module KeyboardControl();
-
+// KeyboardControl Module: Interprets PS/2 keyboard inputs to control directional (up, left, right) and enter actions.
+module KeyboardControl(
+    input clk,           // Main clock
+    input rst,           // Active high reset signal
+    input ps2_clk,       // PS/2 keyboard clock signal for synchronizing data transmission
+    input ps2_data,      // PS/2 keyboard data signal carrying the scan codes
+    output reg up,       // Output for the up direction, set when corresponding scan code is detected
+    output reg left,     // Output for the left direction, set when corresponding scan code is detected
+    output reg right,    // Output for the right direction, set when corresponding scan code is detected
+    output reg enter     // Output for the enter action, set when corresponding scan code is detected
+);
 endmodule
--- Code/sources/UIDisplay.v
+++ Code/sources/UIDisplay.v
@@ -1,3 +1,13 @@
-module UIDisplay();
+// UIDisplay Module: Handles the user interface display for a game, showing the object matrix, score, and next block information on a VGA monitor.
+module UIDisplay(
+    input clk,                       // Main clock for synchronizing the display updates
+    input reset,                     // Active high reset signal to reset the display
+    input [19:0][11:0] objectMatrix, // Object position matrix (20 rows by 12 columns)
+    input [6:0] score,               // Current score
+    input [1:0] nextBlock,           // Type of the next block to be displayed (encoded as 0 to 3)
+    output hsync,                    // Horizontal synchronization signal for VGA
+    output vsync,                    // Vertical synchronization signal for VGA
+    output [7:0] rgb                 // Color channel value for VGA display
+);
 
 endmodule
