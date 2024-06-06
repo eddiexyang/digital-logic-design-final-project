5eda5760732cce6d747a3afdce0467a885b6f8dd
Xingyu Yang <xyyang.cs@gmail.com>
2024-06-06 22:19:49 +0800

    Added flash matrix into the port definition, modified the width of nextBlock

--- Code/sources/UIDisplay.v
+++ Code/sources/UIDisplay.v
@@ -5,7 +5,7 @@ module UIDisplay(
     input [19:0][11:0] objectMatrix, // Object position matrix (20 rows by 12 columns)
     input [19:0][11:0] flash,        // Flash matrix (20 rows by 12 columns)
     input [6:0] score,               // Current score
-    input [2:0] nextBlock,           // Type of the next block to be displayed (encoded as 0 to 3)
+    input [2:0] nextBlock,           // Type of the next block to be displayed (encoded as 0 to 4)
     output hsync,                    // Horizontal synchronization signal for VGA
     output vsync,                    // Vertical synchronization signal for VGA
     output [7:0] rgb                 // Color channel value for VGA display
