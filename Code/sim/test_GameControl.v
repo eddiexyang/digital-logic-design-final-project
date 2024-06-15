72c2d6bcc962c02ef03144dac2ccb529cd9a24c9
Xingyu Yang <xyyang.cs@gmail.com>
2024-06-15 22:28:38 +0800

    Created testbench

---
+++ Code/sim/test_GameControl.v
@@ -0,0 +1,28 @@
+module test_GameControl();
+    reg clk;
+    reg rst;
+    reg [1:0] keyboard_signal;
+    wire [6:0] score;
+    wire [2:0] nextBlock;
+    wire [199:0] objects;
+    
+    always @(*) begin
+        #5; clk <= ~clk;
+    end
+
+    initial begin
+        clk = 0;
+        rst = 1;
+        keyboard_signal = 0;
+    end
+
+    GameControl u_GameControl(
+        .clk             (clk             ),
+        .rst             (rst             ),
+        .keyboard_signal (keyboard_signal ),
+        .score           (score           ),
+        .nextBlock       (nextBlock       ),
+        .objects         (objects         )
+    );
+
+endmodule
